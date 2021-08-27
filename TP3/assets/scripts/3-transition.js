3828e9ce9bfe50241f71047385288e2a9d43fae8
Abdou9696 <abdou.trichili@gmail.com>
2021-08-26 22:37:09 -0400

    data_visualisation

---
+++ TP3/assets/scripts/3-transition.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+* Fichier permettant de gérer la transition entre les données.
+*/
+
+
+/**
+ * Réalise une transition entre les données actuellement utilisées et les nouvelles qui doivent être utilisées.
+ *
+ * @param g       Le groupe SVG dans lequel le graphique à bulles est dessiné.
+ * @param data    Les nouvelles données à utiliser.
+ * @param x       L'échelle pour l'axe X.
+ * @param y       L'échelle pour l'axe Y.
+ * @param r       L'échelle pour le rayon des cercles.
+ */
+function transition(g, data, x, y, r) {
+  /* TODO:
+       - Réaliser une transition entre l'ancienne position et la nouvelle position des cercles.
+       - Mettre à jour la taille du rayon des cercles.
+       - La transition doit se faire en 1 seconde.
+   */
+  g.selectAll("circle")
+    .transition()
+  .attr("r",  function(d,i) {return r(data[i].population); })
+  .attr('cx',  function(d,i) {return x(data[i].lifeExpectancy); })
+  .attr('cy', function(d,i) {return y(data[i].income)})
+
+}
